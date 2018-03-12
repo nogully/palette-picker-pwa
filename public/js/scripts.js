ab7b00ac5e178a7932b7771b47d3fc7e114f8e6e
Nora Gully <nora.gully@gmail.com>
2018-03-12 17:23:06 -0600

    Generating random colors on button click

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,20 @@
 $('button').on('click', loadPalette);
 
+function generateColors() {
+  let colorArray = []
+  for (let i = 0; i <= 5; i++) {
+    colorArray.push('#'+(Math.random()*0xFFFFFF<<0).toString(16))  
+  }
+  return colorArray;
+}
+
 function loadPalette() {
   event.preventDefault();
-  $('#color1').addAttr('style="background-color:blue;"')
+  const colorArray = generateColors();
+  console.log(colorArray)
+  $('#color1').css({ "background-color": `${colorArray[0]}` });
+  $('#color2').css({ "background-color": `${colorArray[1]}` });
+  $('#color3').css({ "background-color": `${colorArray[2]}` });
+  $('#color4').css({ "background-color": `${colorArray[3]}` });
+  $('#color5').css({ "background-color": `${colorArray[4]}` });
 }
