d0c12ca462ab3acb55ec1c9be5aa2d69f3d3517b
Nora Gully <nora.gully@gmail.com>
2018-03-13 22:28:52 -0600

    Style makeover

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,7 +4,7 @@ $('button').on('click', () => loadPalette());
 const generateColors = () => {
   let colorArray = []
   for (let i = 0; i <= 5; i++) {
-    colorArray.push('#'+(Math.random()*0xFFFFFF<<0).toString(16))  
+    colorArray.push('#'+(Math.random()*0xFFFFFF<<0).toString(16));
   }
   return colorArray;
 }
@@ -22,22 +22,19 @@ const loadPalette = () => {
 const loadProjects = async () => {
   const response = await fetch('/api/v1/projects');
   const projects = await response.json();
-  makeMiniPalette(projects[1].name);
+  projects.forEach(project => {
+    makeMiniPalette(project.name, project.id, ['#444', '#CCC', '#BBB', '#555', '#EEE']);
+    $('select').append(`<option value="${project.name}">${project.name}</option>`)
+  })
   
 
 }
 
 const makeMiniPalette = (name, id, colors) => {
   $('#projects').append(`
-    <article>
+    <article class="mini-palette" id=${id}>
       <h4>${name}</h4>
-      <svg id=${id} xmlns="http://www.w3.org/2000/svg">
-        <rect x="00" width="20" height="20" rx="15" ry="15" fill="#6495ED"/>
-        <rect x="20" width="20" height="20" rx="15" ry="15" fill="#FFFFFF"/>
-        <rect x="40" width="20" height="20" rx="15" ry="15" fill="#CCCCCC"/>
-        <rect x="60" width="20" height="20" rx="15" ry="15" fill="#444444"/>
-        <rect x="80" width="20" height="20" rx="15" ry="15" fill="#111111"/>
-      </svg>
+      <div style="background-color:${colors[1]};">${colors[1]}</div>
     </article>
   `)
 }
