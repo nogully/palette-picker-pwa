dc40096e83004e4caf80002c5904900ee2dab4e6
Nora Gully <nora.gully@gmail.com>
2018-03-15 21:24:32 -0600

    First test passing for client route

---
+++ test/routes.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the homepage with text', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200);
+      response.should.be.html;
+    })
+    .catch(err => {
+      throw err;
+    });
+  });
+});
+
+describe('API Routes', () => {
+
+});
